38d1b6489f96cb13a6342b13ca85e5479835ad76
Trajon Felton <trajonfelton@Trajons-Air.lan>
2022-11-02 21:57:13 -0700

    Found out how to count bytes, adding that in, also made test more reliable to read

--- main_test.go
+++ main_test.go
@@ -8,15 +8,23 @@ import (
 func TestCountWords(t *testing.T) {
 	b := bytes.NewBufferString("word1 word2 word3 word4 word5 word6")
 
-	if count(b, false, false) != 6 {
-		t.Errorf("Expected %d got %d instead. \n", 6, count(b, false, false))
+	if exp := count(b, false, false); exp != 6 {
+		t.Errorf("Expected %d got %d instead. \n", 6, exp)
 	}
 }
 
 func TestCountLines(t *testing.T) {
 	b := bytes.NewBufferString("word1 word2 word3 word4 word5 word6")
 
-	if count(b, true, false) != 1 {
-		t.Errorf("Expected %d got %d instead. \n", 6, count(b, true, false))
+	if exp := count(b, true, false); exp != 1 {
+		t.Errorf("Expected %d got %d instead. \n", 6, exp)
+	}
+}
+
+func TestCountBytes(t *testing.T) {
+	b := bytes.NewBufferString("word1 word2 word3 word4 word5 word6")
+
+	if exp := count(b, false, true); exp != 30 {
+		t.Errorf("Expected %d got %d instead. \n", 30, exp)
 	}
 }
