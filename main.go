e39f032b5b5e3d0862a68d2c3b41ecbc0ac58126
Trajon Felton <trajonfelton@Trajons-Air.lan>
2022-11-02 21:40:29 -0700

    Adding function to count bytes through command line

--- main.go
+++ main.go
@@ -2,24 +2,34 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"io"
 	"os"
 )
 
 func main() {
-	fmt.Println(count(os.Stdin))
+	lines, bytes := flag.Bool("l", false, "Count lines"), flag.Bool("b", false, "Count lines")
+
+	flag.Parse()
+	fmt.Println(count(os.Stdin, *lines, *bytes))
 }
 
-func count(r io.Reader) int {
+func count(r io.Reader, countLines bool, countBytes bool) int {
 	scanner := bufio.NewScanner(r)
 
-	scanner.Split(bufio.ScanWords)
+	if !countLines {
+		scanner.Split(bufio.ScanWords)
+	}
 
 	wc := 0
 
 	for scanner.Scan() {
-		wc++
+		if countBytes {
+			wc += len(scanner.Bytes())
+		} else {
+			wc++
+		}
 	}
 
 	return wc
--- main_test.go
+++ main_test.go
@@ -8,7 +8,15 @@ import (
 func TestCountWords(t *testing.T) {
 	b := bytes.NewBufferString("word1 word2 word3 word4 word5 word6")
 
-	if count(b) != 6 {
-		t.Errorf("Expected %d got %d instead. \n", 6, count(b))
+	if count(b, false, false) != 6 {
+		t.Errorf("Expected %d got %d instead. \n", 6, count(b, false, false))
+	}
+}
+
+func TestCountLines(t *testing.T) {
+	b := bytes.NewBufferString("word1 word2 word3 word4 word5 word6")
+
+	if count(b, true, false) != 1 {
+		t.Errorf("Expected %d got %d instead. \n", 6, count(b, true, false))
 	}
 }
